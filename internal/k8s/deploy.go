0b19a8c85ae2f558580ad54a787dec57ed98d537
Aleksey Mavrin <a.mavrin@tinkoff.ru>
2023-04-02 23:20:06 +0300

    getReplicaSetForDeployment: add function

--- internal/k8s/deploy.go
+++ internal/k8s/deploy.go
@@ -2,8 +2,10 @@ package k8s
 
 import (
 	"context"
+	"fmt"
 
 	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
@@ -20,4 +22,54 @@ func GetTargetDeployment(ctx context.Context,
 		return appsv1.Deployment{}, err
 	}
 	return *deployment, nil
+}
+
+func GetPodsForDeployment(ctx context.Context,
+	clientset kubernetes.Interface,
+	namespace string,
+	deployment appsv1.Deployment,
+) ([]corev1.Pod, error) {
+	replicaSet, err := getReplicaSetForDeployment(ctx, clientset, namespace, deployment)
+	if err != nil {
+		return nil, err
+	}
+
+	pods, err := getPodsForReplicaSet(ctx, clientset, namespace, replicaSet)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return pods, nil
+}
+
+func getReplicaSetForDeployment(ctx context.Context,
+	clientset kubernetes.Interface,
+	namespace string,
+	deployment appsv1.Deployment,
+) (appsv1.ReplicaSet, error) {
+	replicaSets, err := clientset.AppsV1().ReplicaSets(namespace).List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return appsv1.ReplicaSet{}, err
+	}
+	for _, rs := range replicaSets.Items {
+		if rs.ObjectMeta.OwnerReferences[0].Kind != "Deployment" {
+			continue
+		}
+		if rs.ObjectMeta.OwnerReferences[0].Name != deployment.ObjectMeta.Name {
+			continue
+		}
+		return rs, nil
+	}
+	return appsv1.ReplicaSet{}, fmt.Errorf("No ReplicaSet found for deployment %s",
+		deployment.ObjectMeta.Name)
+}
+
+func getPodsForReplicaSet(ctx context.Context,
+	clientset kubernetes.Interface,
+	namespace string,
+	replicaSet appsv1.ReplicaSet,
+) ([]corev1.Pod, error) {
+	pods := make([]corev1.Pod, 0)
+	return pods, nil
 }
--- internal/k8s/deploy_test.go
+++ internal/k8s/deploy_test.go
@@ -8,8 +8,29 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-
 	"k8s.io/client-go/kubernetes/fake"
+)
+
+var (
+	deployment1 = appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "deployment1",
+			Namespace: "namespace1",
+		},
+	}
+
+	replicaSet1 = appsv1.ReplicaSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "deployment1-xxxxxx",
+			Namespace: "namespace1",
+			OwnerReferences: []metav1.OwnerReference{
+				{
+					Kind: "Deployment",
+					Name: "deployment1",
+				},
+			},
+		},
+	}
 )
 
 func TestGetTargetDeployment(t *testing.T) {
@@ -28,33 +49,18 @@ func TestGetTargetDeployment(t *testing.T) {
 		{
 			name: "Single deployment in the correct namespace",
 			args: args{
-				ctx: context.Background(),
-				clientset: fake.NewSimpleClientset(&appsv1.Deployment{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "deployment1",
-						Namespace: "namespace1",
-					},
-				}),
+				ctx:            context.Background(),
+				clientset:      fake.NewSimpleClientset(&deployment1),
 				namespace:      "namespace1",
 				deploymentName: "deployment1",
 			},
-			want: appsv1.Deployment{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "deployment1",
-					Namespace: "namespace1",
-				},
-			},
+			want: deployment1,
 		},
 		{
 			name: "No deployment in the correct namespace",
 			args: args{
-				ctx: context.Background(),
-				clientset: fake.NewSimpleClientset(&appsv1.Deployment{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "deployment1",
-						Namespace: "namespace2",
-					},
-				}),
+				ctx:            context.Background(),
+				clientset:      fake.NewSimpleClientset(),
 				namespace:      "namespace1",
 				deploymentName: "deployment1",
 			},
@@ -74,6 +80,53 @@ func TestGetTargetDeployment(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("GetSubjectDeployment() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_getReplicaSetForDeployment(t *testing.T) {
+	type args struct {
+		ctx        context.Context
+		clientset  kubernetes.Interface
+		namespace  string
+		deployment appsv1.Deployment
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    appsv1.ReplicaSet
+		wantErr bool
+	}{
+		{
+			name: "Deployment has ReplicaSer",
+			args: args{
+				ctx:        context.Background(),
+				clientset:  fake.NewSimpleClientset(&deployment1, &replicaSet1),
+				namespace:  "namespace1",
+				deployment: deployment1,
+			},
+			want: replicaSet1,
+		},
+		{
+			name: "No ReplicaSet for Deployment in this namespace",
+			args: args{
+				ctx:       context.Background(),
+				clientset: fake.NewSimpleClientset(&deployment1),
+				namespace: "namespace1",
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := getReplicaSetForDeployment(tt.args.ctx, tt.args.clientset, tt.args.namespace, tt.args.deployment)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("getReplicaSetForDeployment() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getReplicaSetForDeployment() = %v, want %v", got, tt.want)
 			}
 		})
 	}
